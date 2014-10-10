ed2d32f6cb7c3419cb4147e33ebb8920af47876b
Alejandro Tardin <alejandro@tardin.com>
2014-10-10 13:50:27 +0100

    Madness working

--- content/madness.js
+++ content/madness.js
@@ -1,5 +1,10 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
-    // do stuff here
-
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (!window.madness) {
+        window.madness = window.madness = setInterval(function() {
+            $("*").each(function(el) {$(this).css("background-color", "#"+((1<<24)*Math.random()|0).toString(16))});
+        }, 70);
+        $('.fa').addClass('fa-spin');
+    } else {
+        window.location.reload();
+    }
 });
